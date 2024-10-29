9cf30a19a64bcd50adba4e1a7df15b1b7f20fbac
Josh <flinkeydink@gmail.com>
2024-10-29 11:40:00 -0400

    Created prisma functions to register and hash user passwords and login function.

--- prisma/index.js
+++ prisma/index.js
@@ -1,3 +1,25 @@
 const { PrismaClient } = require("@prisma/client");
-const prisma = new PrismaClient();
-module.exports = prisma;
+const bcrypt = require("bcrypt");
+
+const prisma = new PrismaClient().$extends({
+  model: {
+    User: {
+      async register(username, password, firstName, lastName, email) {
+        const hash = await bcrypt.password.hash(password, 10);
+        const user = await prisma.user.create({
+          data: { username, firstName, lastName, email, password: hash },
+        });
+        return user;
+      },
+
+      async login(username, password) {
+        const user = await prisma.user.findUniqueOrThrow({
+          where: { username },
+        });
+        const valid = await bcrypt.compare(password, user.password);
+        if (!valid) throw Error("Invalid password");
+        return user;
+      },
+    },
+  },
+});
