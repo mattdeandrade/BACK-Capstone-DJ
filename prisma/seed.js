4266175e1cf99ece14dac1819e0ab0aa2c43f483
Brandon Hamilton <bham2015@gmail.com>
2024-10-30 11:37:58 -0500

    Wrote pseudocode for seeding in the README file.

---
+++ prisma/seed.js
@@ -0,0 +1,56 @@
+const { faker } = require("@faker-js/faker");
+const prisma = require("../prisma");
+
+const seed = async (
+  numTracks = 50,
+  numUsers = 50,
+  numPlaylists = 10,
+  numEdits = 5
+) => {
+  const users = Array.from({ length: numUsers }, () => ({
+    username: faker.person.fullName(),
+    password: faker.internet.password(),
+    playlists: { create: playlists },
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    email: faker.internet.email(),
+  }));
+
+  const tracks = Array.from({ length: numTracks }, () => ({
+    trackName: faker.music.songName(),
+    artistName: faker.music.artist(),
+    albumName: faker.music.album(),
+    bpm: Math.floor(Math.random() * (200 - 20 + 1)) + 20,
+    genre: faker.music.genre(),
+    instrumental: Math.random() < 0.5,
+    voc: Math.random() < 0.5,
+  }));
+
+  const playlists = Array.from({ length: numPlaylists }, () => ({
+    name: faker.name.zodiac(),
+    description: faker.lorem.sentence(),
+    ownerId: Math.floor(Math.random() * 50) + 1,
+    tracks: { create: tracks },
+  }));
+
+  const playlist = [];
+  for (let i = 0; i < numTracks; i++) {
+    await prisma.track.create({
+      data: tracks,
+    });
+    for (let j = 0; j < numPlaylists; j++) {
+      await prisma.playlist.create({
+        data: playlists,
+      });
+    }
+  }
+
+  await prisma.user.createMany({ data: users });
+  const edits = Array.from({ length: numEdits }, () => ({
+    ownerId: Math.floor(Math.random() * 50) + 1,
+    trackId: Math.floor(Math.random() * numTracks) + 1,
+    trackVocals: Math.random() < 0.5,
+    trackInstrumental: Math.random() < 0.5,
+    time: Math.floor(Math.random() * (3 * 60 * 1000)),
+  }));
+};
