a59016d95ed0a9c194f22127981a67c53dae5aab
Aliyah M Smith <aliyahms@umich.edu>
2024-10-31 21:38:57 -0400

    Succesfully created POST playlist router that includes the array of tracks in playlist. Still need to add if statemnts to make sure all tracks are ownded by the user

--- api/playlists.js
+++ api/playlists.js
@@ -2,10 +2,37 @@ const express = require("express");
 const router = express.Router();
 module.exports = router;
 
+//Authentication Required import
+const { authenticate } = require("./auth/auth");
+
+//Primsa Client import
+const prisma = require("../prisma");
+const { connect } = require("./tracks");
+
 router.get("/", async (req, res, next) => {
   try {
     const playlists = await prisma.track.findMany();
     res.json(playlists);
+  } catch (e) {
+    next(e);
+  }
+});
+
+//Post a new playlist
+router.post("/", authenticate, async (req, res, next) => {
+  const { name, description, trackIds } = req.body;
+  try {
+    const tracks = trackIds.map((id) => ({ id }));
+    const playlist = await prisma.playlist.create({
+      data: {
+        name,
+        description,
+        userId: req.user.id,
+        tracks: { connect: tracks },
+      },
+      include: { tracks: true },
+    });
+    res.status(201).json(playlist);
   } catch (e) {
     next(e);
   }
